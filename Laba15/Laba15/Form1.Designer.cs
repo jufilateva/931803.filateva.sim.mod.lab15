c7dbdf86235ac432efe4b81a18bf3baa29dacff5
jufilateva <74243446+jufilateva@users.noreply.github.com>
2021-06-07 13:12:38 +0500

    Add files via upload

---
+++ Laba15/Laba15/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Laba15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbtime = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbtime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Начальное состояние";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ясно",
+            "облачно",
+            "пасмурно"});
+            this.comboBox1.Location = new System.Drawing.Point(414, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start the modelling";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(282, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Check the statistic";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Minimum = -1D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 133);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 15;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(878, 305);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Время моделирования";
+            // 
+            // tbtime
+            // 
+            this.tbtime.Location = new System.Drawing.Point(154, 12);
+            this.tbtime.Name = "tbtime";
+            this.tbtime.Size = new System.Drawing.Size(79, 20);
+            this.tbtime.TabIndex = 11;
+            this.tbtime.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 450);
+            this.Controls.Add(this.tbtime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbtime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown tbtime;
+    }
+}
+
---
+++ Laba15/Laba15/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba15
+{
+    public partial class Form1 : Form
+    {
+
+        double[,] Q;
+        int n = 5;
+        string[] states = { "ясно", "облачно", "пасмурно" }; string state0;
+        double[] dur = { 0, 0, 0 };
+        int i, inew, j, k; double t, tnew, tau;
+        Random R = new Random();
+
+
+        int I; int time, tt; double p, p1, r;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Q = new double[3, 3];
+
+            Q[0, 0] = -0.5; Q[0, 1] = 0.4; Q[0, 2] = 0.1;
+            Q[1, 0] = 0.3; Q[1, 1] = -0.6; Q[1, 2] = 0.3;
+            Q[2, 0] = 0.1; Q[2, 1] = 0.3; Q[2, 2] = -0.4;
+
+            state0 = (string)comboBox1.SelectedItem;
+            for (k = 0; k < 3; k++) if (states[k].Equals(state0)) I = k;
+        }
+
+        public void thing(int t1)
+        {
+            tt = 0;
+            t = 0;
+
+            while (tt <= time)
+            {
+                tau = Math.Log(R.NextDouble()) / Q[I, I];
+                t += tau;
+
+                p1 = 0;
+                r = R.NextDouble();
+                for (j = 0; j < 3; j++)
+                {
+                    if (j == I) { p = 0; }
+                    else { p = -Q[I, j] / Q[I, I]; }
+                    p1 += p;
+                    if (r < p1) { I = j; break; }
+                }
+
+                if (t >= n)
+                {
+                    chart1.Series[0].Points.AddXY(tt, I);
+                    t = 0;
+                }
+                tt++;
+            }
+        }
+
+        public void but1()
+        {
+            time = (int)tbtime.Value;
+            tt = 0;
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[0].Points.AddXY(tt, I);
+            thing(time);
+        }
+
+        public void but2(double T)
+        {
+            t = 0;
+            while (t <= T)
+            {
+
+                tau = Math.Log(R.NextDouble()) / Q[I, I];
+                t += tau;
+
+                p1 = 0;
+                r = R.NextDouble();
+                for (j = 0; j < 3; j++)
+                {
+                    if (j == I) { p = 0; }
+                    else { p = -Q[I, j] / Q[I, I]; }
+                    p1 += p;
+                    if (r < p1) { I = j; break; }
+                }
+            }
+            k = 0;
+            tnew = t; inew = I;
+            while (k < n)
+            {
+                tau = Math.Log(R.NextDouble()) / Q[I, I];
+                tnew += tau;
+
+                p1 = 0;
+                r = R.NextDouble();
+                for (j = 0; j < 3; j++)
+                {
+                    if (j == I) { p = 0; }
+                    else { p = -Q[I, j] / Q[I, I]; }
+                    p1 += p;
+                    if (r < p1) { I = j; break; }
+                }
+                k++;
+
+                dur[I] += (tnew - t);
+                I = inew; t = tnew;
+            }
+            for (i = 0; i < 3; i++) dur[i] = dur[i] / dur.Sum();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            for (i = 0; i < 3; i++) chart1.Series[1].Points.AddXY(i, dur[i]);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            but1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            double T = 1000;
+            but2(T);
+        }
+    }
+      
+
+       
+    }
+}
